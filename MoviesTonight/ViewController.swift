76f8cfdf0c7f247bb7974937aad9676beac4e153
Pawan <pawanpreetsingh@icloud.com>
2018-09-13 22:02:48 +0530

    Setup Project

---
+++ MoviesTonight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoviesTonight
+//
+//  Created by Pawan on 13/09/18.
+//  Copyright © 2018 Pawan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ MoviesTonightTests/MoviesTonightTests.swift
@@ -0,0 +1,34 @@
+//
+//  MoviesTonightTests.swift
+//  MoviesTonightTests
+//
+//  Created by Pawan on 13/09/18.
+//  Copyright © 2018 Pawan. All rights reserved.
+//
+
+import XCTest
+@testable import MoviesTonight
+
+class MoviesTonightTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
